71acf722198bc191f334b7d977d9ab936ea87860
Manel Sitjar <alu2015071@a22o04.local>
2018-10-09 19:30:04 +0200

    Initial Commit

---
+++ practica1Manel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  practica1Manel
+//
+//  Created by Manel Sitjar on 9/10/18.
+//  Copyright © 2018 Manel Sitjar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
